26e13b656b27f3bb250537eb81718021d4d01dda
Mike <miguelandin@proton.me>
2026-05-30 22:24:31 +0200

    FEAT: chain class & utils

---
+++ chain.py
@@ -0,0 +1,35 @@
+from pygame.math import Vector2
+from math import pi, cos, sin
+from utils import constraint_angle, constraint_distance
+
+
+class Chain:
+    radius: int
+    angles: list[float]
+    max_angle: float
+
+    def __init__(self, origin: Vector2, joint_count: int, radius: int, max_angle: float = 2*pi):
+        self.radius: int = radius
+        self.max_angle: float = max_angle
+        self.joints: list[Vector2] = []
+        self.joints.append(origin)
+        self.angles: list[float] = []
+        self.angles.append(0)
+
+        space = Vector2(0, self.radius)
+        for i in range(1, joint_count):
+            self.joints.append(Vector2(self.joints[i-1] + space))
+            self.angles.append(pi/2)
+
+    def new_head(self, pos: Vector2):
+        self.angles[0] = (pos-self.joints[0]).angle_rad
+        self.joints[0] = pos
+
+        for i in range(1, len(self.joints)):
+            current_displacement = (self.joints[i-1]-self.joints[i])
+            current_angle = current_displacement.angle_rad
+            self.angles[i] = constraint_angle(
+                current_angle, self.angles[i-1], self.max_angle)
+
+            self.joints[i] = self.joints[i-1] - \
+                Vector2(cos(self.angles[i]), sin(self.angles[i])) * self.radius
---
+++ utils.py
@@ -0,0 +1,31 @@
+from math import pi
+from pygame import Vector2
+
+
+# el ángulo no pasa de 2π
+def simplify_angle(angle: float) -> float: return angle % (2*pi)
+
+
+# calcula la ruta angular mas corto entre dos ángulos
+def calc_angle_diff(angle: float, anchor: float) -> float: return pi - \
+    simplify_angle(angle+pi-anchor)
+
+
+# la ruta angular no pasa del ángulo constraint
+def constraint_angle(angle: float, anchor: float, constraint: float) -> float:
+    angle_diff = calc_angle_diff(angle, anchor)
+    if abs(angle_diff) <= constraint:
+        return simplify_angle(angle)
+    if angle_diff > constraint:
+        return simplify_angle(anchor - constraint)
+    return simplify_angle(anchor + constraint)
+
+
+# los dos puntos siempre estarán en la misma distancia
+def constraint_distance(pos: Vector2, anchor: Vector2, constraint: float):
+    displacement = pos - anchor
+    if displacement.length() == 0:
+        return anchor.copy()
+
+    displacement.scale_to_length(constraint)
+    return anchor + displacement
